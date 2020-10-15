653390398bf1b688beb4832ffd942754652cbfe0
Michal Atlas <misa.31415@gmail.com>
2020-10-15 14:38:05 +0200

    Solved Task#4

--- Task 4 - Multiplication/MultiplicationHelper.cs
+++ Task 4 - Multiplication/MultiplicationHelper.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Linq;
 
 namespace Multiplication
 {
     public static class MultiplicationHelper
     {
+        private static int[] tripleMax(int[] input)
+        {
+            int f = int.MinValue, s = int.MinValue, t = int.MinValue;
+            foreach (var num in input)
+            {
+                if (num > f)
+                {
+                    t = s;
+                    s = f;
+                    f = num;
+                }
+                else if (num > s)
+                {
+                    t = s;
+                    s = num;
+                }
+                else if (num > t)
+                {
+                    t = num;
+                }
+            }
+
+            return new int[]{f, s, t};
+        }
+        private static int[] doubleMin(int[] input)
+        {
+            int f = int.MaxValue, s = int.MaxValue;
+            foreach (var num in input)
+            {
+                if (num < f)
+                {
+                    s = f;
+                    f = num;
+                }
+                else if (num < s)
+                {
+                    s = num;
+                }
+            }
+
+            return new int[]{f, s};
+        }
+
         public static int GetHighestProductOfThree(int[] input)
         {
-            return 0;
+            if (input.Length < 3){throw new ArgumentException();}
+            if (input.Length == 3)
+            {
+                return input.Aggregate(1, (a, b) => a * b);
+            }
+
+            int[] maxes = tripleMax(input);
+            int[] mins = doubleMin(input);
+            
+            return Math.Max(maxes.Aggregate(1, (a,b)=>a*b), maxes[0] * mins[0] * mins[1]);
+            
         }
     }
 }
