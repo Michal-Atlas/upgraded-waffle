6ecf97c4cd7a8dd31a8f5c635fae7637da13c4b0
Michal Atlas <michal.z.atlas@gmail.com>
2020-11-06 17:16:54 +0100

    Solved Task #6

--- Task 6 - WordCloud/WordCloudEngine.cs
+++ Task 6 - WordCloud/WordCloudEngine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace WordCloud
 {
@@ -12,9 +13,36 @@ namespace WordCloud
             CreateCloud(sentence);
         }
 
+        private string FirstUpper(string word)
+        {
+            return word[0].ToString().ToUpper() + word.Substring(1);
+        }
         private void CreateCloud(string sentence)
         {
-            // Count the frequency of each word
+            var sep = sentence.Split(new []{",",".", "?", "!"," -", "- "," ", "\u2026", "...", "\r\n", "\n"}, StringSplitOptions.None);
+            foreach (var word in sep)
+            {
+                if(word.Length > 0)
+                {
+                    if (word == word.ToLower()&&Words.ContainsKey(FirstUpper(word)))
+                    {
+                        var cache = Words[FirstUpper(word)];
+                        Words.Remove(FirstUpper(word));
+                        Words[word] = cache+1;
+                        
+                    }else if (word != word.ToLower() && Words.ContainsKey(word.ToLower()))
+                    {
+                        Words[word.ToLower()]++;
+                    }
+                    else
+                    {
+                        if (Words.ContainsKey(word))
+                        {
+                            Words[word]++;
+                        }else{Words.Add(word, 1);}
+                    }
+                }
+            }
         }
     }
 }
