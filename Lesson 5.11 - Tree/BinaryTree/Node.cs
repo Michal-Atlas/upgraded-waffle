171d7a61d78772499a813496193d69bbbcd5e2d7
Michal Atlas <michal.z.atlas@gmail.com>
2020-11-06 17:18:17 +0100

    BinaryTree: Added ToString Method to Node

--- Lesson 5.11 - Tree/BinaryTree/Node.cs
+++ Lesson 5.11 - Tree/BinaryTree/Node.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
-
-namespace BinaryTree
+﻿namespace BinaryTree
 {
     public class Node
     {
         public int Value;
-        public Node LeftChild { get; set; }
-        public Node RightChild { get; set; }
-
 
         public Node(int value, Node leftChild = null, Node rightChild = null)
         {
             Value = value;
             LeftChild = leftChild;
             RightChild = rightChild;
+        }
+
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+
+        public override string ToString()
+        {
+            return Value + "{" + (LeftChild != null ? LeftChild.ToString() : "-") +
+                   (RightChild != null ? RightChild.ToString() : "-") + "}";
         }
     }
 }
